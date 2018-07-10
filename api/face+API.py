22257ea739776ae1ee888a8ca2215158d0b149d7
rajyam403 <rajyam403@gmail.com>
2018-07-10 18:25:36 +0530

    face detection

---
+++ api/face+API.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+import requests
+# If you are using a Jupyter notebook, uncomment the following line.
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+from PIL import Image
+from matplotlib import patches
+from io import BytesIO
+
+# Replace <Subscription Key> with your valid subscription key.
+subscription_key = "7bb6c4a79387414b9cb2ad739dab6551"
+assert subscription_key
+
+# You must use the same region in your REST call as you used to get your
+# subscription keys. For example, if you got your subscription keys from
+# westus, replace "westcentralus" in the URI below with "westus".
+#
+# Free trial subscription keys are generated in the westcentralus region.
+# If you use a free trial subscription key, you shouldn't need to change
+# this region.
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+
+# Set image_url to the URL of an image that you want to analyze.
+image_url = 'https://www.filmibeat.com/fanimg/soundarya-photos-images-39891.jpg'
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+    'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+}
+data = {'url': image_url}
+response = requests.post(face_api_url, params=params, headers=headers, json=data)
+faces = response.json()
+
+# Display the original image and overlay it with the face information.
+image = Image.open(BytesIO(requests.get(image_url).content))
+plt.figure(figsize=(8, 8))
+ax = plt.imshow(image, alpha=0.6)
+for face in faces:
+    fr = face["faceRectangle"]
+    fa = face["faceAttributes"]
+    origin = (fr["left"], fr["top"])
+    p = patches.Rectangle(
+        origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+    ax.axes.add_patch(p)
+    plt.text(origin[0], origin[1], "%s, %d, %s, %s, %s, %s"%(fa["gender"].capitalize(), fa["age"],fa["headPose"],fa["smile"],fa["facialHair"],fa["glasses"]),
+             fontsize=20, weight="bold", va="bottom")
+_ = plt.axis("off")
+
